f41ec03381b9f7d8aed23d653e10784d9bbcbbad
Tony Sloane <inkytonik@gmail.com>
2015-04-22 10:17:46 +1000

    Add config and command-line options that control printing of IML trees rather than always doing it

--- src/main/scala/au/edu/mq/comp/perentiemq/Main.scala
+++ src/main/scala/au/edu/mq/comp/perentiemq/Main.scala
@@ -1,19 +1,34 @@
 package au.edu.mq.comp.perentiemq
 
-import org.kiama.util.Compiler
 import iml.IMLSyntax.Program
+import org.kiama.util.{CompilerWithConfig, Config}
 
-trait Driver extends Compiler[Program] {
+abstract class IMLConfig (args : Array[String]) extends Config (args) {
+    lazy val sourcePrint = opt[Boolean] ("srcprint", short = 's',
+                                         descr = "Print the source tree")
+    lazy val sourcePrettyPrint = opt[Boolean] ("srcprettyprint", short = 'p',
+                                               descr = "Pretty-print the source tree")
+}
+
+trait Driver extends CompilerWithConfig[Program,IMLConfig] {
 
     import iml.IMLPrettyPrinter.{any, pretty}
     import java.io.Reader
     import org.kiama.output.PrettyPrinterTypes.Document
-    import org.kiama.util.Config
+    import org.kiama.util.{Emitter, ErrorEmitter, OutputEmitter}
+
+    override def createConfig (args : Array[String],
+                               out : Emitter = new OutputEmitter,
+                               err : Emitter = new ErrorEmitter) : IMLConfig =
+        new IMLConfig (args) {
+            lazy val output = out
+            lazy val error = err
+        }
 
     // Not using Scala parser library
     var parser = null
 
-    override def makeast (reader : Reader, filename : String, config : Config) : Either[Program,String] = {
+    override def makeast (reader : Reader, filename : String, config : IMLConfig) : Either[Program,String] = {
         val p = new iml.IML (reader, filename)
         val pr = p.pProgram (0)
         if (pr.hasValue)
@@ -22,9 +37,14 @@ trait Driver extends Compiler[Program] {
             Right (p.format (pr.parseError))
     }
 
-    override def process (filename : String, ast : Program, config : Config) {
-        config.error.emitln (pretty (any (ast)).layout)
-        config.error.emit (format (ast).layout)
+    override def process (filename : String, ast : Program, config : IMLConfig) {
+
+        if (config.sourcePrint ())
+            config.error.emitln (pretty (any (ast)).layout)
+
+        if (config.sourcePrettyPrint ())
+            config.error.emit (format (ast).layout)
+
     }
 
     def format (ast : Program) : Document =
--- src/test/scala/au/edu/mq/comp/perentiemq/iml/IMLTests.scala
+++ src/test/scala/au/edu/mq/comp/perentiemq/iml/IMLTests.scala
@@ -1,16 +1,17 @@
 package au.edu.mq.comp.perentiemq.iml
 
-import au.edu.mq.comp.perentiemq.Driver
+import au.edu.mq.comp.perentiemq.{Driver, IMLConfig}
 import IMLSyntax.Program
-import org.kiama.util.TestCompiler
+import org.kiama.util.TestCompilerWithConfig
 
 /**
  * Tests that check that the IML parser and pretty-printers build
  * the correct trees that pretty-print to the correct output.
  */
-class IMLTests extends Driver with TestCompiler[Program] {
+class IMLTests extends Driver with TestCompilerWithConfig[Program,IMLConfig] {
 
     val path = "src/test/scala/au/edu/mq/comp/perentiemq/iml"
-    filetests ("IML parse and print", path, ".iml", ".out")
+    filetests ("IML parse and print", path, ".iml", ".out",
+               argslist = List (Array ("-p", "-s")))
 
 }
