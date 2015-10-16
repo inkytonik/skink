4d915e855a7d8c7f8bdd9870e201807111ef6c20
Franck Cassez <franck.cassez@mq.edu.au>
2015-10-17 09:10:51 +1100

    Change assert in one file from programs/simple

--- programs/simple/simple-loop-correct.c
+++ programs/simple/simple-loop-correct.c
@@ -1,8 +1,13 @@
+/*
+ this one is incorrect I think: if r < 0 at the beginning
+ the last assert will not hold
+ */
+
 int main () {
   int r;
   while (r > 0) {
-      __VERIFIER_assert(r > 0);
-      r = r - 1;
+    __VERIFIER_assert(r > 0);
+    r = r - 1;
   }
   __VERIFIER_assert(r == 0);
 }
