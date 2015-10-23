b9af71d86817567e6e47c8fbe776e21cf67d1db3
Tony Sloane <inkytonik@gmail.com>
2015-10-23 10:19:51 -0500

    Add standard __VERIFIER_assert definition and __VERIFIER_error declaration to simple programs so they are in same format as SV-COMP ones

--- programs/simple/simple-function-correct.c
+++ programs/simple/simple-function-correct.c
@@ -1,3 +1,12 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+    ERROR: __VERIFIER_error();
+  }
+  return;
+}
+
 int func (int i)
 {
     __VERIFIER_assert(i > 0);
