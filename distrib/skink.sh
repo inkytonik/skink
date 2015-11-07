b4b4f1eed0b52311b752d30db8cb3a97611962ef
Franck Cassez <franck.cassez@mq.edu.au>
2015-11-08 07:21:21 +1100

    Minor edit in README.md file (in a comment)

--- distrib/skink.sh
+++ distrib/skink.sh
@@ -12,7 +12,7 @@ clangargs="-c -emit-llvm -g -o - -S -x c $clangdefs $clangwargs"
 # compile with clang and discard the warnings
 $CC $clangargs $1 2>/dev/null | $OPT -S -inline -o $llfile 
 
-# solver is Z3, maximum iteration 20, timeout for solver 20s
+# solver is Z3, maximum iteration 20, timeout for solver
 java -jar $skinkdir/skink-v1.0.jar -v -eZ3 -m20 -o60 $llfile | tee  $wtnfile 
 
 # remove log file
