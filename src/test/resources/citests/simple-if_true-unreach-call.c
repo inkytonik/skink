5edbd81d1aa9f1a0f22d2e12825bfabcfcc6c316
Matt Roberts <matthew.roberts@mq.edu.au>
2016-06-09 09:25:45 +1000

    found a few more tests that will pass at the moment and updated the .gitignore file to ignore the .ll files that are generated in the test folder when we process the .c files.

---
+++ src/test/resources/citests/simple-if_true-unreach-call.c
@@ -0,0 +1,17 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+    ERROR: __VERIFIER_error();
+  }
+  return;
+}
+
+int main ()
+{
+   int i, j;
+   if (i > j)
+     __VERIFIER_assert (i > j);
+   if (i > j)
+     __VERIFIER_assert (i > j);
+}
