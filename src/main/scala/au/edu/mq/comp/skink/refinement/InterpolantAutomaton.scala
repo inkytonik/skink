9ddbeb512522f54cb7ce47a9ce06d431c0a4e9df
Franck Cassez <franck.cassez@mq.edu.au>
2016-10-26 23:51:48 +1100

    Add back edges to linear interpolant automaton where possible.

--- src/main/scala/au/edu/mq/comp/skink/refinement/InterpolantAutomaton.scala
+++ src/main/scala/au/edu/mq/comp/skink/refinement/InterpolantAutomaton.scala
@@ -18,11 +18,6 @@ object InterpolantAuto extends Resources with Commands with Core {
     val logger = getLogger(this.getClass)
     val itpLogger = getLogger(this.getClass, ".itp")
 
-    // import au.edu.mq.comp.automat.lang.Lang
-    // import au.edu.mq.comp.automat.util.Determiniser.toDetNFA
-    // import au.edu.mq.comp.skink.ir.{FailureTrace, IRFunction, Trace}
-    // import au.edu.mq.comp.skink.Skink.{getLogger, toDot}
-
     /**
      * Make an interpolant automaton for the given trace choices. For now, we just
      * generate a simple linear automaton so the refinement process will remove
@@ -35,7 +30,6 @@ object InterpolantAuto extends Resources with Commands with Core {
         NFA(Set(0), transitions.toSet, Set(choices.length))
     }
 
-    // import au.edu.mq.comp.skink.ir.llvm.LLVMFunction
     import au.edu.mq.comp.skink.ir.IRFunction
     import au.edu.mq.comp.smtlib.interpreters.{SMTLIB2Interpreter}
     import au.edu.mq.comp.smtlib.solvers._
@@ -56,9 +50,7 @@ object InterpolantAuto extends Resources with Commands with Core {
     ) : NFA[Int, Int] = {
 
         val linearAuto : NFA[Int, Int] = buildAutoForTrace(choices)
-
-        //  Collect partition of indices according to blockNames
-        val indexPartition = function.traceToRepetitions(Trace(choices))
+        itpLogger.info(s"Linear Interpolant automaton built")
 
         //  From the sequence of choices, retrieve the sequence of terms that encodes the semantics
         //  of the trace
@@ -81,14 +73,15 @@ object InterpolantAuto extends Resources with Commands with Core {
                     //  should be UnSat()
                     case Success(UnSat()) =>
                         assert(namedTerms.size >= 2)
+                        itpLogger.info(s"Solver returned UnSat as expected")
 
                         //  now get interpolants
                         getInterpolants(namedTerms.head, namedTerms.tail.head, namedTerms.drop(2) : _*)
 
                     //  if not UnSat, log the result and exit
-                    case Success(sat) =>
-                        itpLogger.error(s"Solver did not return UnSat while computing interpolants: $sat")
-                        sys.error(s"Computation of interpolant automaton failed. Solver returned $sat")
+                    case Success(other) =>
+                        itpLogger.error(s"Solver did not return UnSat while computing interpolants: $other")
+                        sys.error(s"Computation of interpolant automaton failed. Solver returned $other")
 
                     case Failure(f) =>
                         itpLogger.error(s"Solver failed to determine sat-status in InterpolantAuto $f")
@@ -105,6 +98,11 @@ object InterpolantAuto extends Resources with Commands with Core {
                 //  add the first and last trivial interpolants
                 val completeItp = True() +: xitp :+ False()
 
+                //  Collect partition of indices according to block equivalence
+                val indexPartition : Seq[Seq[Int]] = function.traceToRepetitions(Trace(choices))
+                itpLogger.info(s"Partitions $indexPartition");
+                itpLogger.info(s"Non-singleton partitions ${indexPartition.filter(_.size > 1)}");
+
                 /**
                  * Check if backedges can be added to the linear automaton
                  * If there is a repetition of a block at index i and j, we
@@ -113,21 +111,29 @@ object InterpolantAuto extends Resources with Commands with Core {
                  * backedge. For instance if the a set is {1,4,7} (same block at index 1,4,7)
                  * we try to add a backedge from 4 to 2 with choices(1)
                  */
-
-                val newBackEdges = for (
-                    p <- indexPartition if (p.size > 1);
-                    i = p.head min p.tail.head;
-                    j = p.head max p.tail.head;
-                    tgtItp = completeItp(i + 1);
-                    srcItp = completeItp(j);
-                    res = function.checkPrePost(
-                        srcItp.unIndexed,
-                        Trace(choices),
-                        tgtItp.unIndexed
-                    ) if res
-                ) yield {
-                    (j ~> (i + 1))(choices(i))
-                }
+                val newBackEdges =
+                    for (
+                        p <- indexPartition if (p.size > 1);
+                        i = p.head min p.tail.head;
+                        j = p.head max p.tail.head;
+                        tgtItp = completeItp(i + 1);
+                        srcItp = completeItp(j);
+                        x1 = srcItp.unIndexed;
+                        x2 = tgtItp.unIndexed;
+                        if using(new Z3 with QF_AUFLIA) {
+                            implicit solver =>
+                                function.checkPost(
+                                    x1,
+                                    Trace(choices),
+                                    index = j,
+                                    choice = choices(i),
+                                    x2
+                                )
+                        } == Success(true)
+                    ) yield {
+                        itpLogger.info(s"new backedge found from $j to ${i + 1} with choice $i")
+                        (j ~> (i + 1))(choices(i))
+                    }
 
                 //  add the new edges to the linearAuto basis
                 NFA(
@@ -135,6 +141,7 @@ object InterpolantAuto extends Resources with Commands with Core {
                     linearAuto.transitions ++ newBackEdges,
                     linearAuto.accepting
                 )
+
 
             case Failure(f) =>
                 itpLogger.error(s"Solver could note compute interpolants $f")
