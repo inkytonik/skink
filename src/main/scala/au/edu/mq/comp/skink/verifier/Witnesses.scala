2fe3b116575f87ad933ff3320762e3d45241221e
Matt Roberts <matthew.roberts@mq.edu.au>
2016-11-23 15:24:31 +1100

    another update to the witness files to support evolving competition requirements

--- src/main/scala/au/edu/mq/comp/skink/verifier/Witnesses.scala
+++ src/main/scala/au/edu/mq/comp/skink/verifier/Witnesses.scala
@@ -80,26 +80,53 @@ class Witnesses(config : SkinkConfig) {
                     s"$node$edge"
             }
 
+        def digestOfFile(filename : String) : String = {
+            import java.io.File
+            import java.io.FileInputStream
+            import java.security.MessageDigest
+            import java.security.Security
+
+            val file = new File(filename)
+            val buffer = Array.ofDim[Byte](file.length().toInt)
+            val fis = new FileInputStream(file)
+            fis.read(buffer)
+            fis.close()
+            val md = MessageDigest.getInstance("SHA-1")
+            md.update(buffer)
+            javax.xml.bind.DatatypeConverter.printHexBinary(md.digest()).toLowerCase()
+        }
+
         val witness =
             s"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
                |<graphml xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
                |         xmlns="http://graphml.graphdrawing.org/xmlns"
                |>
                |
-               |<key id="entry"         for="node"  attr.name="entry"           attr.type="boolean"><default>false</default></key>
-               |<key id="block"         for="node"  attr.name="block"           attr.type="int"   />
-               |<key id="node.src"      for="edge"  attr.name="sourcecode"      attr.type="string"/>
-               |<key id="edge.src"      for="node"  attr.name="sourcecode"      attr.type="string"/>
-               |<key id="startline"     for="edge"  attr.name="startline"       attr.type="int"   />
-               |<key id="endline"       for="edge"  attr.name="endline"         attr.type="int"   />
-               |<key id="violation"     for="node"  attr.name="isViolationNode" attr.type="boolean"><default>false</default></key>
-               |<key id="specification" for="graph" attr.name="specification"   attr.type="string"/>
-               |<key id="producer"      for="graph" attr.name="producer"        attr.type="string"/>
-               |<key id="programfile"   for="graph" attr.name="programFile"     attr.type="string"/>
+               |<key id="entry"          for="node"  attr.name="entry"            attr.type="boolean"><default>false</default></key>
+               |<key id="block"          for="node"  attr.name="block"            attr.type="int"   />
+               |<key id="node.src"       for="edge"  attr.name="sourcecode"       attr.type="string"/>
+               |<key id="edge.src"       for="node"  attr.name="sourcecode"       attr.type="string"/>
+               |<key id="startline"      for="edge"  attr.name="startline"        attr.type="int"   />
+               |<key id="endline"        for="edge"  attr.name="endline"          attr.type="int"   />
+               |<key id="violation"      for="node"  attr.name="isViolationNode"  attr.type="boolean"><default>false</default></key>
+               |<key id="witness-type"   for="graph" attr.name="witness-type"     attr.type="string"/>
+               |<key id="sourcecodelang" for="graph" attr.name="sourcecodelang"   attr.type="string"/>
+               |<key id="producer"       for="graph" attr.name="producer"         attr.type="string"/>
+               |<key id="specification"  for="graph" attr.name="specification"    attr.type="string"/>
+               |<key id="programfile"    for="graph" attr.name="programFile"      attr.type="string"/>
+               |<key id="programhash"    for="graph" attr.name="programhash"      attr.type="string"/>
+               |<key id="memorymodel"    for="graph" attr.name="memorymodel"      attr.type="string"/>
+               |<key id="architecture"   for="graph" attr.name="architecture"     attr.type="string"/>
                |
                |<graph edgedefault="directed">
-               |  <data key="producer">skink</data>
-               |  <data key="specification">CHECK( init(main()), LTL(G ! call(__VERIFIER_error())) )</data>
+               |  <data key="witness-type"  >violation_witness</data>
+               |  <data key="sourcecodelang">C</data>
+               |  <data key="producer"      >skink</data>
+               |  <data key="specification" >CHECK( init(main()), LTL(G ! call(__VERIFIER_error())) )</data>
+               |  <data key="programfile"   >${config.filenames.getOrElse(List[String]("unknown")).head}</data>
+               |  <data key="programhash"   >${config.filenames.map(_.head).map(digestOfFile(_)).getOrElse("0000000000000000000000000000000000000000")}</data>
+               |  <data key="memorymodel"   >simple</data>
+               |  <data key="architecture"  >${config.integerSize.map(_.toString).getOrElse("32")}bit</data>
                |
                |${nodesAndEdges.mkString("\n")}
                |</graph>
