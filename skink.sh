f3fd6604f2a45fc3b5d2a2eaba283e87432588a4
Matt Roberts <matthew.roberts@mq.edu.au>
2016-11-25 17:19:50 +1100

    updates to CI tests to reflect new witnesses

--- skink.sh
+++ skink.sh
@@ -13,15 +13,10 @@ veriffile=$base.verif
 
 skinkdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-java -jar $skinkdir/target/scala-2.11/skink-assembly-2.0-SNAPSHOT.jar -f C -v -eZ3 -m20 $* | tee $veriffile
+java -Xmx1400m -Xss5m -cp ./:target/scala-2.11/skink-assembly-2.0-SNAPSHOT.jar au.edu.mq.comp.skink.Main --verify $* | tee $veriffile
 
 case `head -1 $veriffile` in
     FALSE*)
         tail -n +2 $veriffile > $wtnfile
-        if [ "$SKINK_DEBUG" == "yes" ]; then
-        	echo " " >> $skinkdir/witness_checks
-        	echo $1 >> $skinkdir/witness_checks
-        	$skinkdir/programs/cpacheck "$( dirname $1)"/ALL.prp $1 $wtnfile >> $skinkdir/witness_checks
-        fi
         ;;
 esac
