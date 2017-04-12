4970457f22c604c9dcbe5a1d01e944a3af274c62
Tony Sloane <tony.sloane@me.com>
2017-04-13 08:17:47 +1000

    Fix error in witness XML spec where node.src and edge.src were applicable to the wrong entries

--- src/main/scala/au/edu/mq/comp/skink/verifier/Witnesses.scala
+++ src/main/scala/au/edu/mq/comp/skink/verifier/Witnesses.scala
@@ -58,8 +58,8 @@ class Witnesses(config : SkinkConfig) {
            |
            |<key id="entry"          for="node"  attr.name="entry"            attr.type="boolean"><default>false</default></key>
            |<key id="block"          for="node"  attr.name="block"            attr.type="int"   />
-           |<key id="node.src"       for="edge"  attr.name="sourcecode"       attr.type="string"/>
-           |<key id="edge.src"       for="node"  attr.name="sourcecode"       attr.type="string"/>
+           |<key id="node.src"       for="node"  attr.name="sourcecode"       attr.type="string"/>
+           |<key id="edge.src"       for="edge"  attr.name="sourcecode"       attr.type="string"/>
            |<key id="startline"      for="edge"  attr.name="startline"        attr.type="int"   />
            |<key id="endline"        for="edge"  attr.name="endline"          attr.type="int"   />
            |<key id="violation"      for="node"  attr.name="isViolationNode"  attr.type="boolean"><default>false</default></key>
