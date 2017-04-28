25a5a777f38471b07e9daa00eb840c78dd708c19
Tony Sloane <tony.sloane@me.com>
2017-04-28 15:55:26 +1000

    Fix latest_libs script
    - use “git” user for public repos
    - add expect-for-scala library which is now separate from expect

--- latest_libs.sh
+++ latest_libs.sh
@@ -4,9 +4,9 @@ if cd kiama; then hg pull -u && cd -; else hg clone https://bitbucket.org/inkyto
 cd kiama; sbt "++2.12.2 publish-local"; sbt "++2.12.2 test:publish-local"; cd -
 if cd scalallvm; then hg pull -u && cd -; else hg clone https://$1:$2@bitbucket.org/inkytonik/scalallvm; fi
 cd scalallvm; sbt publish-local; cd -
-if cd automat; then git pull && cd -; else git clone https://$1:$2@bitbucket.org/franck44/automat.git; fi
+if cd automat; then git pull && cd -; else git clone https://git@bitbucket.org/franck44/automat.git; fi
 cd automat; sbt publish-local; cd -
-if cd expect; then git pull && cd -; else git clone https://$1:$2@bitbucket.org/franck44/expect.git; fi
-cd expect; sbt publish-local; cd -
-if cd mq-scala-smtlib; then git pull && cd -; else git clone https://$1:$2@bitbucket.org/franck44/mq-scala-smtlib.git; fi
+if cd expect-for-scala; then git pull && cd -; else git clone https://bitbucket.org/franck44/expect-for-scala; fi
+cd expect-for-scala; sbt publish-local; cd -
+if cd mq-scala-smtlib; then git pull && cd -; else git clone https://git@bitbucket.org/franck44/mq-scala-smtlib.git; fi
 cd mq-scala-smtlib; sbt publish-local; cd -
