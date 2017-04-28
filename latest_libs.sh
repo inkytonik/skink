d08ebbb189e4534735a7b8d069bdaccf51dd1230
Tony Sloane <tony.sloane@me.com>
2017-04-28 14:52:18 +1000

    Make latest_libs use Kiama for 2.12

--- latest_libs.sh
+++ latest_libs.sh
@@ -1,7 +1,7 @@
 if cd sbt-rats; then hg pull -u && cd -; else hg clone https://bitbucket.org/inkytonik/sbt-rats; fi
 cd sbt-rats; sbt publish-local; cd -
 if cd kiama; then hg pull -u && cd -; else hg clone https://bitbucket.org/inkytonik/kiama; fi
-cd kiama; sbt "++2.11.8 publish-local"; sbt "++2.11.8 test:publish-local"; cd -
+cd kiama; sbt "++2.12.2 publish-local"; sbt "++2.12.2 test:publish-local"; cd -
 if cd scalallvm; then hg pull -u && cd -; else hg clone https://$1:$2@bitbucket.org/inkytonik/scalallvm; fi
 cd scalallvm; sbt publish-local; cd -
 if cd automat; then git pull && cd -; else git clone https://$1:$2@bitbucket.org/franck44/automat.git; fi
