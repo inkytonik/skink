ad34d1b54d839a1da5fb8ad1a3890ac27d75663c
Tony Sloane <tony.sloane@me.com>
2017-11-08 14:45:01 +1100

    Move to BigDecimal for real witness values in nondet witnesses

--- src/main/scala/au/edu/mq/comp/skink/verifier/NonDetWitnesses.scala
+++ src/main/scala/au/edu/mq/comp/skink/verifier/NonDetWitnesses.scala
@@ -13,27 +13,27 @@ class NonDetWitnesses(config : SkinkConfig) extends Witnesses(config) {
     import org.bitbucket.franck44.scalasmt.parser.SMTLIB2Syntax._
     import org.bitbucket.franck44.scalasmt.typedterms.Value
 
-    def termToFloat(term : Term) : Float =
+    def termToBigDecimal(term : Term) : BigDecimal =
         term match {
             case ConstantTerm(DecLit(v)) =>
-                java.lang.Float.valueOf(v)
+                BigDecimal(v)
             case NegTerm(ConstantTerm(DecLit(v))) =>
-                -1 * java.lang.Float.valueOf(v)
+                -1 * BigDecimal(v)
             case ConstantTerm(NumLit(v)) =>
-                java.lang.Float.valueOf(v)
+                BigDecimal(v)
             case NegTerm(ConstantTerm(NumLit(v))) =>
-                -1 * java.lang.Float.valueOf(v)
+                -1 * BigDecimal(v)
             case _ =>
-                sys.error(s"termToFloat: unexpected first arg of RealDivTerm: $term")
+                sys.error(s"termToBigDecimal: unexpected term: $term")
         }
 
     def optValueToCValue(optValue : Option[Value]) : String =
         if (optValue.isDefined)
             optValue.get.t match {
                 case RealDivTerm(dt, List(nt)) =>
-                    val dv = termToFloat(dt)
-                    val nv = termToFloat(nt)
-                    s"${dv / nv}"
+                    val dv = termToBigDecimal(dt)
+                    val nv = termToBigDecimal(nt)
+                    (dv / nv).toString
                 case ConstantTerm(DecLit(s))                        => s
                 case ConstantTerm(HexaLit(s))                       => s"${new java.math.BigInteger(s, 16).intValue()}"
                 case ConstantTerm(NumLit(i))                        => i.toString
