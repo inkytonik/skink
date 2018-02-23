6b00ca8ed9f2280e1094aef23a60103b75d363a0
Tony Sloane <tony.sloane@me.com>
2018-02-23 15:03:09 +1100

    Put all configurations back in

--- src/main/scala/au/edu/mq/comp/skink/verifier/Verifier.scala
+++ src/main/scala/au/edu/mq/comp/skink/verifier/Verifier.scala
@@ -58,10 +58,10 @@ class Verifier(ir : IR, config : SkinkConfig) {
             import scala.collection.mutable.StringBuilder
 
             val argSets = List(
-                List("-e", "Z3", "-i", "bit", "-r", "bit") //,
-            // List("-e", "Z3", "-i", "bit"),
-            // List("-e", "Z3"),
-            // List("-e", "Yices-nonIncr")
+                List("-e", "Z3", "-i", "bit", "-r", "bit"),
+                List("-e", "Z3", "-i", "bit"),
+                List("-e", "Z3"),
+                List("-e", "Yices-nonIncr")
             )
 
             val unknownReasons = new StringBuilder()
