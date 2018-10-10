01d17fbfbce84f58a9e3e5530f92e1adc7a4d573
Tony Sloane <tony.sloane@me.com>
2018-10-10 11:09:59 +1100

    Remove distinction between math and bit CI tests since we are going all bitvector

--- src/test/scala/au/edu/mq/comp/skink/CITests.scala
+++ src/test/scala/au/edu/mq/comp/skink/CITests.scala
@@ -24,33 +24,20 @@ package au.edu.mq.comp.skink
 import au.edu.mq.comp.skink.ir.IR
 import org.bitbucket.inkytonik.kiama.util.TestCompilerWithConfig
 
-trait CITests extends Driver with TestCompilerWithConfig[IR, SkinkConfig] {
-    val witnessFormats = List("trace", "nondet")
+class CITests extends Driver with TestCompilerWithConfig[IR, SkinkConfig] {
+
+    def makeTests(optLevel : Int, subpath : String = "") = {
+        val path = s"src/test/resources/citests$subpath"
+        val args = List(List("-v", "-w", "-", s"-O$optLevel"))
+        filetests(s"citests", path, "_true-unreach-call.c", s"_true-unreach-call.verif", argslist = args)
+        filetests(s"citests", path, "_false-unreach-call.c", s"_false-unreach-call.O$optLevel.verif", argslist = args)
+    }
+
     val optLevels = List(0, 2)
-
-    def makeTests(path : String, optLevel : Int, witnessFormat : String, extraArgs : List[String] = List()) = {
-        val args = List(List("-v", "-w", "-", s"-O$optLevel", "-W", witnessFormat) ++ extraArgs)
-        filetests(s"citests ($path)", s"src/test/resources/citests/$path", "_true-unreach-call.c",
-            s"_true-unreach-call.verif", argslist = args)
-        filetests(s"citests ($path)", s"src/test/resources/citests/$path", "_false-unreach-call.c",
-            s"_false-unreach-call.O$optLevel.$witnessFormat.verif", argslist = args)
+    for (optLevel <- optLevels) {
+        makeTests(optLevel)
     }
-}
+    makeTests(2, "/function-Yices-nonIncr")
+    makeTests(2, "/function-Z3")
 
-class MathCITests extends CITests {
-    for (witnessFormat <- witnessFormats) {
-        for (optLevel <- optLevels) {
-            makeTests("math", optLevel, witnessFormat)
-        }
-        makeTests(s"math/function-Yices-nonIncr", 2, witnessFormat)
-        makeTests(s"math/function-Z3", 2, witnessFormat)
-    }
-}
-
-class BitCITests extends CITests {
-    for (witnessFormat <- witnessFormats) {
-        for (optLevel <- optLevels) {
-            makeTests("bit", optLevel, witnessFormat)
-        }
-    }
 }
