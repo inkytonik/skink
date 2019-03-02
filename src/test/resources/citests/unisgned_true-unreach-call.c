fbf3bf7084d2c65da9cd51f220ef334d160e8846
Tony Sloane <tony.sloane@me.com>
2019-03-02 11:00:55 +1100

    Add simple CITest for unsigned nondet

---
+++ src/test/resources/citests/unisgned_true-unreach-call.c
@@ -0,0 +1,9 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern unsigned int __VERIFIER_uint();
+
+int main(void) {
+  unsigned int i = __VERIFIER_nondet_uint();
+
+  if (i < 0)
+    __VERIFIER_error();
+}
