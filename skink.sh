8ab26f1d9b442be0fba6c11c3341987f6913a360
Tony Sloane <tony.sloane@me.com>
2019-08-25 13:17:53 +1000

    skink.sh enhancements
    - remove unused code
    - pass extra options through to skink
    - support for profiling using async-profiler
    
    https://github.com/jvm-profiling-tools/async-profiler

--- skink.sh
+++ skink.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# skink.sh
+
+# Pfofiling support requires installation of async-profiler:
+# https://github.com/jvm-profiling-tools/async-profiler
+# You need to set the following variable to the location of the agent shared library.
+PROFILER=async-profiler-1.5-macos-x64/build/libasyncProfiler.so
 
 if test $# -lt 1
 then
-  echo "usage: skink.sh (--version | file.c)"
+  echo "usage: skink.sh (--version | (--profile flat|tree|svg)? [skink options] file.c)"
   exit 1
 fi
 
@@ -24,8 +30,14 @@ else
   fi
 fi
 
+PROFARG=""
+PROFOUT=""
+PROFTYPE=""
+
 function run() {
-  java -Xmx1400m -Xss16m \
+  java \
+    $PROFARG \
+    -Xmx1400m -Xss16m \
     -cp ./:$JAR \
     au.edu.mq.comp.skink.Main \
     $*
@@ -35,15 +47,38 @@ if test "$1" == "--version"
 then
   run --version
 else
-  file=$1
-  base=${file%.*}
-  IWTNFILE=$file.graphml
-  WTNFILE=witness.graphml
-
-  run --verify --output-witness --witness-file $WTNFILE $file
-
-  if test -e $WTNFILE
+  if test "$1" == "--profile"
   then
-    cp $WTNFILE $IWTNFILE
+    shift
+    if test $# -gt 0
+    then
+      case $1 in
+        flat) PROFOUT="profile.txt"; PROFTYPE="flat,file=$PROFOUT";;
+        svg)  PROFOUT="profile.svg"; PROFTYPE="svg,file=$PROFOUT";;
+        tree) PROFOUT="profile.html"; PROFTYPE="tree,file=$PROFOUT";;
+        *)
+          echo "skink.sh: --profile expected flat, tree, or svg, got $1"
+          exit 1
+      esac
+      shift
+      if test -e $PROFILER
+      then
+        PROFARG="-agentpath:$PROFILER=start,$PROFTYPE"
+      else
+        echo "skink.sh: profiler library doesn't exist: $PROFILER"
+        exit 1
+      fi
+    else
+      echo "skink.sh: --profile expects additional argument: flat, tree, or svg"
+      exit 1
+    fi
   fi
+
+  run --verify --output-witness --witness-file witness.graphml $*
+
+  if test "$PROFOUT" != ""
+  then
+    echo "skink.sh: profile output is in $PROFOUT"
+  fi
+
 fi
